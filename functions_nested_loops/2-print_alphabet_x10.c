47065828bed89ce9fd0634b06bce82ecb1dfa34d
Bevan Tan <btan400@gmail.com>
2025-02-22 06:45:01 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print alphabet x 10
+ *Description: a to z using _putchar user defined header x 10
+ *
+ *Return: return (0) in the 2-main.c
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	y = 0;
+
+	{
+	x = 'a';
+		while (x <= 'z')
+		{
+		_putchar(x);
+		x = x + 1;
+		}
+
+	_putchar('\n');
+	y = y + 1;
+	}
+}
