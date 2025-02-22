0b7757127040b1f5660f57b04686010a9f414f82
Bevan Tan <btan400@gmail.com>
2025-02-22 06:31:47 +0000

    1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
  *main - print alphabet
  *Description: a to z using _putchar user defined header
  *
- *Return: Always 0 (sucess) dont need return (0) because???? - this is actually the command, and it has return (0) in the 1-main.c
+ *Return: return (0) in the 1-main.c
  */
 
 void print_alphabet(void)
