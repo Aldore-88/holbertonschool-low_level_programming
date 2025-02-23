e22852a770d0680d67ff8624fd026af8607af2e9
3-islower <btan400@gmail.com>
2025-02-23 12:58:59 +0000

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,25 +10,34 @@ void times_table(void)
 	int row;
 	int prod;
 
-	col = 0;
-
-	while (col <= 9)
-	{
 	row = 0;
 
-		while (row <= 9)
+	while (row <= 9)
+	{
+		col = 0;
+
+		while (col <= 9)
 		{
-		prod = row * col;
-		if (prod > 9)
-		{
-			_putchar((prod / 10) + '0');
+			prod = row * col;
+			if (prod > 9)
+			{
+				_putchar((prod / 10) + '0');
+			}
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+	
+			_putchar((prod % 10) + '0');
+
+			if (col < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+				col = col + 1;
 		}
-		_putchar((prod % 10) + '0');
-		_putchar(',');
-		_putchar(' ');
-		row = row + 1;
-		}
-	col = col + 1;
+	row = row + 1;
 	_putchar('\n');
 	}
 }
