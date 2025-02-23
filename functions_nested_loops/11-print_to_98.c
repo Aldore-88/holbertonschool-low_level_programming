8e3eaab20efa27fa44c9f33329ea9d2f7b85e643
3-islower <btan400@gmail.com>
2025-02-23 13:37:55 +0000

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints number until 98
+ * @n: starting number
+ *
+ *
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n = n + 1;
+	}
+	while (n > 98)
+	{
+		printf("%d, ",n);
+
+		n = n - 1;
+	}
+	if (n == 98)
+		printf("%d \n", n);
+}
