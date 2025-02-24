eb95c126412c056fc7073431435afd2eba2c0185
Bevan Tan <btan400@gmail.com>
2025-02-24 04:11:02 +0000

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,28 +11,25 @@ int main(void)
 
 	i = 1;
 
-	while (i < 100)
+	while (i <= 100)
 	{
-		if (i % 15 == 0 && i > 0)
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
-			i = i + 1;
 		}
-		if (i % 5 == 0 && i > 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-			i = i + 1;
 		}
-		if (i % 3 == 0 && i > 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
-			i = i + 1;
 		}
 		else
 		{
 			printf("%d ", i);
-			i = i + 1;
 		}
+		i = i + 1;
 	}
 
 	return (0);
