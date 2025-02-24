d1324d7520b9460ad1a8cbca1ade8c4f98d77eb7
Bevan Tan <btan400@gmail.com>
2025-02-24 05:49:50 +0000

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle
+ *
+ *
+ */
+void print_triangle(int size)
+{
+	int col = 0;
+	int row;
+
+	while (col < size)
+	{
+		row = 0;
+
+		while (row < size)
+		{
+			if ((row + 1) < (size - col))
+			{
+				_putchar('.');
+				row = row + 1;
+			}
+			else
+			{
+				_putchar('#');
+				row = row + 1;
+			}
+		}
+
+		_putchar('\n');
+		col = col + 1;
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
