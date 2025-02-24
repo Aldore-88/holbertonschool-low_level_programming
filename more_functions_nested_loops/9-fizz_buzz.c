4dbbdff75ca029f0c740c83d696ca2f2e133253a
Bevan Tan <btan400@gmail.com>
2025-02-24 03:54:46 +0000

    9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - counting to 100 
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 100)
+	{
+		if (i % 15 == 0)
+		{
+			printf ("FizzBuzz ");
+			i = i + 1;
+		}
+		if (i % 5 == 0)
+		{
+			printf ("Buzz ");
+			i = i + 1;
+		}
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			i = i + 1;
+		}
+		else
+		{
+			printf("%d ", i);
+			i = i + 1;
+		}
+	}
+
+	return (0);
+}
