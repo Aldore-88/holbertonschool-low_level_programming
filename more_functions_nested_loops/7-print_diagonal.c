3cb54d2b2cd35513d0def5f1e12c3a41a746dc95
Bevan Tan <btan400@gmail.com>
2025-02-24 04:38:48 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,33 +2,27 @@
 /**
  * print_diagonal - prints line '\' in a diagonal n times
  * @n: number of times to print line
- *
+ * Description: listing out '\' first then think about the spaces
  */
 void print_diagonal(int n)
 {
+	int row = 0;
 	int space;
-	
-	space = 0;
-	if (n > 0)
+
+	while (row < n)
 	{
-		while (space <= n)
+		space = 0;
+		while (space < row)
 		{
-			_putchar ('.');
-			if (space == n)
-			{
-				_putchar (92);
-				_putchar ('\n');
-				space = space + 1;
-			}
-			else
-			{
-				_putchar (' ');
-				space = space + 1;
-			}
+			_putchar(' ');
+			space = space + 1;
 		}
+		_putchar(92);
+		_putchar('\n');
+		row = row + 1;
 	}
-	else
+	if (n <= 0)
 	{
-		_putchar ('\n');
+		_putchar('\n');
 	}
 }
