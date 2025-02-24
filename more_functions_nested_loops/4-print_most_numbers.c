0ddd04768071362352934f9d44bdbfb558e12c68
Bevan Tan <btan400@gmail.com>
2025-02-24 01:01:34 +0000

    4-print_most_numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0 - 9, but not 2 or 4
+ *
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 2)
+		{
+			_putchar (i + '0');
+		}
+	}
+	_putchar ('\n');
+}
