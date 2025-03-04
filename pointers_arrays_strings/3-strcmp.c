3015b3890af91e2e2ece28914516824a52215567
Bevan Tan <btan400@gmail.com>
2025-03-04 04:06:39 +0000

    3-strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcmp - string compare
+ *@s1: string 1
+ *@s2: stirng 2
+ *Return: difference in string, else if same = 0
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int total = 0;
+
+	while (s1[i] != '\0' && total == '0')
+	{
+		return (s1[i] - s2[i]);
+		i = i + 1;
+	}
+	return (s1[i] - s2[i]);
+}
