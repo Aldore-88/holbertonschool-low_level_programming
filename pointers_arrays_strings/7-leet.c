d026a51156f35923424fc7cd61b9ddf7ce39b5b6
Bevan Tan <btan400@gmail.com>
2025-03-04 06:36:22 +0000

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if(str[i] = 'a' || str[i] = 'A')
+			str[i] = '4';
+
+		if(str[i] = 'e' || str[i] = 'E')
+			str[i] = '3';
+
+if 
+	}
+}
