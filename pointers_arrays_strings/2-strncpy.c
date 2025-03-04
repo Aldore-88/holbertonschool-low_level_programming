7df175e8e5bb8d2732f7afcbb3512d71f25e9f77
Bevan Tan <btan400@gmail.com>
2025-03-04 03:13:41 +0000

    2-strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,4 +5,22 @@
  *
  *
  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
 
+	while (i < n)
+	{
+		if (src[i] > 0)
+		{
+			dest[i] = src[i];
+			i = i + 1;
+		}
+		else
+		{
+			dest[i] = '\0';
+			i = i + 1;
+		}
+	}
+	return (dest);
+}
