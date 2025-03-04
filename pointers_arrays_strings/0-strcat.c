c54ec2673b3675cd773add0b09c1ddb4f3e4bda4
Bevan Tan <btan400@gmail.com>
2025-03-04 00:06:45 +0000

    0-strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strcat - string catalate? put strings together
+ *@dest: pointer destination
+ *@src: pointer source
+ */
+char *_strcat(char *dest,char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i = i + 1;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j = j + 1;
+		i = i + 1;
+	}
+	return(dest);
+}
