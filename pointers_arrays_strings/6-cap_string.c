434ffa6c2a6b5caf5a177419663e19d201ce2b2a
Bevan Tan <btan400@gmail.com>
2025-03-04 05:49:53 +0000

    6-cap

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * cap_string - capitalise first letter of word
+ * @str: string
+ *
+ *
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z' && str[i - 1] == ' ')
+		{
+			str[i] = str[i] - 32;
+		}
+		i = i + 1;
+	}
+	return (str);
+}
