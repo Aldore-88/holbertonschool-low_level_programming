4a4381e6adcf381bedd41f224e1c2f2bdc1822e9
Bevan Tan <btan400@gmail.com>
2025-03-06 04:58:16 +0000

    0-memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * _memset - memory set
+ * @s: array set
+ * @b: character to place into memory
+ * @n: unsigned int, gives the no of memory spaces to set
+ * Return: returns the array s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
