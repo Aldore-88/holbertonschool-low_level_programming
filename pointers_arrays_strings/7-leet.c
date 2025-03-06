742fd7d01db58a1d3b8b04080a2977744b8357d8
Bevan Tan <btan400@gmail.com>
2025-03-06 04:32:35 +0000

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,22 +1,29 @@
 #include "main.h"
 /**
- *
- *
- *
+ * leet - change string to leet speak
+ * @str: string
+ * Return: return string as leet speek
  *
  */
 char *leet(char *str)
 {
 	int i = 0;
+	int j = 0;
+	char alph[] = "aAeEoOtTlL";
+	char replace[] = "43071";
 
 	while (str[i] != '\0')
 	{
-		if(str[i] = 'a' || str[i] = 'A')
-			str[i] = '4';
-
-		if(str[i] = 'e' || str[i] = 'E')
-			str[i] = '3';
-
-if 
+		j = 0;
+		while (j < 10)
+		{
+			if (alph[j] == str[i])
+			{
+				str[i] = replace[j / 2];
+			}
+			j = j + 1;
+		}
+		i = i + 1;
 	}
+	return (str);
 }
