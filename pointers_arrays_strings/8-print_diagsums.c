e4b93831e56a163fa914da525ad3b567f3c5fb3a
Bevan Tan <btan400@gmail.com>
2025-03-08 08:28:05 +0000

    8-print_diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int pos = 0;
+	int rev_pos = size-1;
+	int total = 0;
+	int rev_total = 0;
+
+	while (pos < size)
+	{
+		total = total + a[pos + (size * pos)];
+
+		rev_total = rev_total + a[rev_pos + (size * pos)];
+		
+		rev_pos = rev_pos - 1;
+		pos = pos + 1;
+	}
+	printf("%d, %d\n", total, rev_total);
+}
