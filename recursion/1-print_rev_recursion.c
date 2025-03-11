57ec6a29aca7ce0f849240e57e50de67e4d8892b
Bevan Tan <btan400@gmail.com>
2025-03-11 00:15:14 +0000

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,13 +1,20 @@
 #include "main.h"
 /**
- *
- *
- *
+ * _print_rev_recursion - prints string in reverse
+ * @s: string
+ * Return: when the string ends it starts to unwind the recursive function
  *
  */
 void _print_rev_recursion(char *s)
 {
-	_putchar(_print_rev_recursion(*s) + 1);
-	s++;
+	if (s[0] != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[0]);
+	}
 
+	if (s[0] == '\0')
+	{
+		return;
+	}
 }
