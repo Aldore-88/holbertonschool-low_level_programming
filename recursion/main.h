b9c6d78e205473a9aad3e75f6f1b53ba513ce050
Bevan Tan <btan400@gmail.com>
2025-03-11 01:22:42 +0000

    4-pow

--- recursion/main.h
+++ recursion/main.h
@@ -4,7 +4,8 @@
 int _putchar(char);
 void _puts_recursion(char *);
 void _print_rev_recursion(char *);
-int _strlen_recursion(char *s);
-int factorial(int n);
+int _strlen_recursion(char *);
+int factorial(int);
+int _pow_recursion(int, int);
 
 #endif
