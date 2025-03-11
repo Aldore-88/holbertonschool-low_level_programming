5307f0ce0b09cd48bbe5e24e8f8ae639a6c35f26
Bevan Tan <btan400@gmail.com>
2025-03-11 03:00:22 +0000

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * prime_check - checking if number x is a prime
+ * @x: integer to check if prime
+ * @num: incrementing number to check against prime
+ * Return: return if number is a prime
+ *
+ */
 int prime_check(int x, int num)
 {
 	if (x < 2)
@@ -7,6 +13,7 @@ int prime_check(int x, int num)
 		return (0);
 	}
 	if ((x % num == 0) && (num < x))
+	/*prime number is only divisible by itself*/
 	{
 		return (0);
 	}
@@ -17,10 +24,10 @@ int prime_check(int x, int num)
 	return (prime_check(x, num + 1));
 }
 /**
- * is_prime_number - prime is a number that is 
+ * is_prime_number - prime is a number that is
  * not divisibile by anything other than itself
- *
- *
+ * @n: value to check
+ * Return: returns the prime_check
  *
  */
 int is_prime_number(int n)
