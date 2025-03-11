e1af7376e1013eb4044aa86b761a47b111d820df
Bevan Tan <btan400@gmail.com>
2025-03-11 05:31:14 +0000

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
 /**
- *
- *
- *
+ * main - main function -> unused declaration
+ * @argc: counts number of contents from commandline
+ * @argv: values of each input from commandline
+ * Return: return 0
  */
 
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
