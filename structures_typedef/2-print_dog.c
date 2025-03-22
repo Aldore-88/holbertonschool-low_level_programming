67cd93305c9059222f39f195afdbc2f77c25bc76
Bevan Tan <btan400@gmail.com>
2025-03-22 03:57:51 +0000

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,6 +8,22 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == 0)
+	{
+		return;
+	}
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->age < 0)
+	{
+		d->age = 0;
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
