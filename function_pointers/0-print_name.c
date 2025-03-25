d229abea15ac9b1604b3757be11845daac4a6621
Bevan Tan <btan400@gmail.com>
2025-03-25 02:14:44 +0000

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
+ * print_name - prints name using function poitner
+ * @name: name of to input into function
+ * @f: function pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
