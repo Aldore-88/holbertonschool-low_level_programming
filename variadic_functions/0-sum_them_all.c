ac406da925635355bbde021178641a5efc0e38db
Bevan Tan <btan400@gmail.com>
2025-03-27 03:34:01 +0000

    0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list ptr;
+
+	va_start(ptr, n);
+	i = 0;
+	sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	while (i < n)
+	{
+		sum = sum + va_arg(ptr);
+		i = i + 1;
+	}
+	va_end(ptr);
+	return (sum);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef _VARIADIC_H
+#define _VARIADIC_H
+
+int sum_them_all(const unsigned int, ...);
+
+
+#endif
