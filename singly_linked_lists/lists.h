a670b8a67b30cc1e2b8a407d3aeca321793d5299
Bevan Tan <btan400@gmail.com>
2025-04-03 06:13:05 +0000

    2-add_node.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -11,12 +11,12 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *);
-size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
 
 #endif
