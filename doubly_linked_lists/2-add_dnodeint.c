a388da65147e90e77277263b76af852f230ed367
Bevan Tan <btan400@gmail.com>
2025-04-15 05:44:05 +0000

    2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -32,10 +32,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (head != NULL)
 	{
-		head = new_node;
+		*head = new_node;
 	}
 
-	*head = new_node; /**/
+	*head = new_node;
 
 	return (new_node);
 	free(new_node);
