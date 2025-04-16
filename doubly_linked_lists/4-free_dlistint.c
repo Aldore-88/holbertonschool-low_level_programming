3a8a4bc3a8ab3fd3a62f3276fd75aee14864f825
Bevan Tan <btan400@gmail.com>
2025-04-16 08:52:42 +0000

    4-free_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -6,7 +6,6 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-/*temp node to hold next from head, free head, move to next node. prev node of ->next will need to be move to temp??*/
 	dlistint_t *temp_node;
 
 	while (head != NULL)
