9f22fc1b7071906ccd48c73a0b3c78a2a084f1ad
Bevan Tan <btan400@gmail.com>
2025-04-16 09:02:37 +0000

    5-get_dnodeint.c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get node at index number
+ * @head: head node
+ * @index: index at which to get node
+ *
+ * Return: return node_temp at index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node_temp;
+	unsigned int count;
+
+	node_temp = head;
+	count = 0;
+
+	while (count < index)
+	{
+		node_temp = node_temp->next;
+		count = count + 1;
+	}
+	return (node_temp);
+}
