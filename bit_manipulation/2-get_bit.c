d0a6510d5f3b3ee0e4d5e3c3b32291295aba9027
Bevan Tan <btan400@gmail.com>
2025-04-21 06:28:38 +0000

    2-get_bit.c

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_bit - get bit at index
+ * @n: number
+ * @index: index of binary digit
+ *
+ * Return: n & 1, value of last digit using & operator
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int count;
+
+	count = 0;
+
+	while (count < index)
+	{/*shift bits up to index*/
+		n = n >> 1;
+		count = count + 1;
+	}
+	return (n & 1); 
+}
